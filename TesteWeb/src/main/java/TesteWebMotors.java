90ba91789b0eeef6109e7f97a9fd96b6eba92eef
Guto Soares <gutosoares18@hotmail.com>
2021-04-05 14:14:30 -0300

    testeQA

--- TesteWeb/src/main/java/TesteWebMotors.java
+++ TesteWeb/src/main/java/TesteWebMotors.java
@@ -18,33 +18,23 @@ public class TesteWebMotors {
 		driver.findElement(By.id("searchBar")).sendKeys("Honda");
 		Assert.assertEquals("Honda", driver.findElement(By.id("searchBar")).getAttribute("value"));
 			
-	//	driver.quit();
-	}
-	
-	
-	
-	@Test
-	public void teste2 () {
-	
-		WebDriver driver = new FirefoxDriver();
+
 		driver.get("https://www.webmotors.com.br/carros/estoque?tipoveiculo=carros&estadocidade=estoque");
 		System.getProperty("webdriver.gecko.driver","C:\\Users\\Guto Soares\\Downloads\\drive");
 		driver.findElement(By.id("searchBar")).sendKeys("City");
 		Assert.assertEquals("City", driver.findElement(By.id("searchBar")).getAttribute("value"));
    
-		//	driver.quit();
-	
-	}
-	
-	@Test
-	public void teste3 () {
-	
-		WebDriver driver = new FirefoxDriver();
+		
 		driver.get("https://www.webmotors.com.br/carros/estoque?tipoveiculo=carros&estadocidade=estoque");
 		System.getProperty("webdriver.gecko.driver","C:\\Users\\Guto Soares\\Downloads\\drive");
 		driver.findElement(By.id("searchBar")).sendKeys("City 1.5 Automatico");
 		Assert.assertEquals("City 1.5 Automatico", driver.findElement(By.id("searchBar")).getAttribute("value"));
  
-       //  driver.quit();
+       
+		driver.get("https://www.webmotors.com.br/carros/estoque?tipoveiculo=carros&estadocidade=estoque");
+		System.getProperty("webdriver.gecko.driver","C:\\Users\\Guto Soares\\Downloads\\drive");
+		driver.findElement(By.id("searchBar")).sendKeys(" Honda " + " City " );
+		
+			
 	}
 }
